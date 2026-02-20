753425ee45c3c1599a54c5e5c5df0c7c1ee96539
Aashu Prasad <aashuprasad047@gmail.com>
2026-02-20 22:34:47 +0530

    Update DefaultCons.java

--- OOPs/Constructor/DefaultCons.java
+++ OOPs/Constructor/DefaultCons.java
@@ -1,1 +1,15 @@
 
+class DC {
+    DC() {
+        System.out.println("Default Constructor");
+    }
+}
+
+public class DefaultCons {
+    public static void main(String[] args) {
+
+        DC d = new DC();
+
+    }
+}
+
