bacfaf12db152c93d5bd7a2b6556975b5ef31e6c
Aashu Prasad <aashuprasad047@gmail.com>
2026-02-20 13:57:35 +0530

    Added Var.java program

--- day_01/Var.java
+++ day_01/Var.java
@@ -1,1 +1,13 @@
+public class Var {
 
+    public static void main(String[] args) {
+
+        int age = 20;
+        String name = "Ashu";
+        double marks = 85.5;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+    }
+}
