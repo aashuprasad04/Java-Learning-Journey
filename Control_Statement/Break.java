06afa82be0b2d30baa84aa9a57e3230910391002
Aashu Prasad <aashuprasad047@gmail.com>
2026-02-20 15:02:23 +0530

    Update Break.java

--- Control_Statement/Break.java
+++ Control_Statement/Break.java
@@ -1,6 +1,7 @@
 public class Break {
     public static void main(String[] args){
 
+        // break statement used in loop
         for(int i=0; i<=5; i++){
             System.out.println(i);
             if(i==3){
@@ -8,6 +9,16 @@ public class Break {
             }
         }
 
+        // break statement used in swith case
+        int i=1;
+        switch(i){
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+        }
+
     }
 }
-
