de2bd75478b16625dd864577eb9416accc378ed7
Aashu Prasad <aashuprasad047@gmail.com>
2026-02-20 22:06:59 +0530

    Update and rename Class.java to Class_Object.java

---
+++ OOPs/Class_Object/Class_Object.java
@@ -0,0 +1,14 @@
+class A {
+    void displayClassA() {
+        System.out.println("A");
+    }
+}
+
+public class ClassAndObject {
+    public static void main(String[] args) {
+
+        A a = new A();
+        a.displayClassA();
+
+    }
+}
