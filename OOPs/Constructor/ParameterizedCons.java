00ba229e3f6402928c3ed799a2e766626f857fda
Aashu Prasad <aashuprasad047@gmail.com>
2026-02-21 09:29:33 +0530

    Update ParameterizedCons.java

--- OOPs/Constructor/ParameterizedCons.java
+++ OOPs/Constructor/ParameterizedCons.java
@@ -1,1 +1,22 @@
+class PC {
+    int age;
+    String name;
 
+    PC(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    void display() {
+        System.out.println("Age : " + age + "\n" + "Name : " + name);
+    }
+}
+
+public class ParameterizedCons {
+    public static void main(String[] args) {
+
+        PC p = new PC(20, "Azy");
+        p.display();
+
+    }
+}
