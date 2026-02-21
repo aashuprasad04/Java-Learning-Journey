f9d5247bd7559e7c7e348d95d24370beb3e26e9c
Aashu Prasad <aashuprasad047@gmail.com>
2026-02-21 09:51:56 +0530

    Update OverloadByNumber.java

--- OOPs/Method_Overloading/OverloadByNumber.java
+++ OOPs/Method_Overloading/OverloadByNumber.java
@@ -1,1 +1,23 @@
+class A {
+
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class OverloadByNumber {
+    public static void main(String[] args) {
+
+        A a = new A();
+        int m = a.sum(1, 2);
+        System.out.println(m);
+
+        int n = a.sum(1, 2, 3);
+        System.out.println(n);
+    }
+}
 
