bd64468dd7b5d22f428867192e25ad720deeaaf9
Aashu Prasad <aashuprasad047@gmail.com>
2026-02-23 08:50:34 +0530

    Create forEachLoop.java

---
+++ Control_Statement/forEachLoop.java
@@ -0,0 +1,11 @@
+public class forEachLoop {
+    public static void main(String[] args) {
+        // System.out.println("hello");
+
+        String[] A = { "a", "b", "c" };
+        for (String AA : A) {
+            System.out.println(AA);
+        }
+
+    }
+}
